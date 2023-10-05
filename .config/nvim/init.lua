366f5e4e3a1d76c42cb6a9c88431f12e56bc4026
Chris Hooke <chooke@unca.edu>
2023-10-05 11:02:14 -0400

    changed nvim lualine theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -184,7 +184,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'auto',
+        theme = 'powerline_dark',
         component_separators = '|',
         section_separators = '',
       },

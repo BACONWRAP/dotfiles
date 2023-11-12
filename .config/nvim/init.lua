ba6f490c5c9180e9d83e13c03a97ee65b7f08243
Chris Hooke <chooke@unca.edu>
2023-11-12 14:46:30 -0500

    added custom keymap file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,6 +25,8 @@ vim.opt.rtp:prepend(lazypath)
 --
 --  You can also configure plugins after the setup call,
 --    as they will be available in your neovim runtime.
+require('custom.core.keymap')
+
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
@@ -534,4 +536,3 @@ vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true,
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-vim.keymap.set('n', '<leader>W', '<c-w>', { desc = 'change [W]indow' })

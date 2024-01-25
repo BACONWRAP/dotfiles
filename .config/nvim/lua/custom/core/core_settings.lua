bb78a0f2bbf5d15d06296d214c77667a07e3e3a4
Chris Hooke <chooke@unca.edu>
2024-01-25 15:34:10 -0500

    some adjustments to improve core usability

--- .config/nvim/lua/custom/core/core_settings.lua
+++ .config/nvim/lua/custom/core/core_settings.lua
@@ -1,3 +1,5 @@
-vim.opt_global.spell = true
-vim.opt_global.spelllang = 'en_us'
-vim.opt_global.scrolloff = 5 -- Minimal number of screen lines to keep above and below the cursor. (default: 0)
+vim.o.spell = true
+vim.o.spelllang = 'en_us'
+vim.o.scrolloff = 5 -- Minimal number of screen lines to keep above and below the cursor. (default: 0)
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
